ada9eaada3b4e85d98f6b00c1292313d09e14f8b
karthick1213 <karthickc1213@gmail.com>
2021-10-14 18:23:21 +0530

    THIRD COMMIT

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require('dotenv').config()
+// API's
+const Book = require("./API/book");
+const Author = require("./API/author");
+const Publication = require("./API/publication");
+
+// Import database
+const Database = require("./database");
+
+// Connecting Mongoose
+mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+    }
+).then(() => console.log("connection extablished!"))
+ .catch((error) => console.log(error));
+// Initialization
+const OurApp = express();
+OurApp.use(express.json())
+// Microservices
+OurApp.use("/book", Book);
+OurApp.use("/author", Author);
+OurApp.use("/publication", Publication);
+// Main page
+OurApp.get("/", (request, response) => {
+    return response.json({ message: "Server is working!!!"})
+});
+// Hosting
+OurApp.listen(5000, () => console.log("Server is running"));
